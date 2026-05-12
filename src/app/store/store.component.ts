5661e2115ed1e765a7e411ccd0ddef196ddc1649
rajshri <rajshrichandel@gmail.com>
2026-05-12 13:57:46 +0100

    Add product catalogue pagination

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -11,18 +11,44 @@ import { ProductRepository } from "../model/product.repository";
 })
 export class StoreComponent {
   public selectedCategory: string | null = null;
+  public productsPerPage = 4;
+  public selectedPage = 1;
+
 
   constructor(private repository: ProductRepository) {}
 
   get products(): Product[] {
-    return this.repository.getProducts(this.selectedCategory);
+    const pageIndex = (this.selectedPage - 1) * this.productsPerPage; 
+    
+    return this.repository.getProducts(this.selectedCategory)
+    .slice(pageIndex, pageIndex + this.productsPerPage);
   }
 
   get categories(): string[] {
     return this.repository.getCategories();
   }
 
+  get pageNumbers(): number[] {
+    return Array(
+      Math.ceil(
+        this.repository.getProducts(this.selectedCategory).length /this.productsPerPage
+      )
+    )
+      .fill(0)
+      .map((Value, index) => index + 1);
+  }
+
   changeCategory(newCategory: string | null): void {
     this.selectedCategory = newCategory;
+    this.changePage(1);
+  }
+
+  changePage(newPage: number): void {
+    this.selectedPage = newPage;
+  }
+
+  changePageSize(newSize: number): void {
+    this.productsPerPage = Number(newSize);
+    this.changePage(1);
   }
 }
